e8d96319f897a6805762ff1f0bfa57ac0536ab64
CAIRO\MEssam <messam@cairo.aas.com.sa>
2020-01-20 08:32:24 +0200

    Create Logs in Database

--- src/main/java/com/log4j/Log4jApplication.java
+++ src/main/java/com/log4j/Log4jApplication.java
@@ -10,10 +10,11 @@ public class Log4jApplication {
 	private static Logger logger = Logger.getLogger(Log4jApplication.class);
 
 	public static void main(String[] args) throws InterruptedException {
-        PropertyConfigurator.configure("F:\\eclipse - last version\\workshop\\MavenTestProject\\src\\main\\resources\\log4j.xml");
-		for (long i = 1; i <= 2000; i++) {
-			logger.info("This is the " + i + " time I say 'Hello World'.");
-			//Thread.sleep(100);
-		}
+        PropertyConfigurator.configure("F:\\eclipse - last version\\workshop\\MavenTestProject\\src\\main\\resources\\log4j.properties");
+        
+        logger.debug("Sample debug message");
+        logger.info("Sample info message");
+        logger.error("Sample error message");
+        logger.fatal("Sample fatal message");
 	}
 }
